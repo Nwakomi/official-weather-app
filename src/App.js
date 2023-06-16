a0fa6a6504826d877876b2a75b2d3edfdaa51ffd
Nwakomi <natali.krmz@gmail.com>
2023-06-16 19:29:33 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Hello and welcome</h1>
 <footer>
 This project was coded by Natali and is 
     <a href="https://github.com/Nwakomi/official-weather-app" target="_blank"> open-sourced in Github</a>
     </footer>
+    </div>
     </div>
   );
 }
