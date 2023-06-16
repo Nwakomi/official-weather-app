d92b307bf55702cb270c9ec1537e23b060446ff8
Nwakomi <natali.krmz@gmail.com>
2023-06-16 16:16:35 +0200

    added the github repo url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Hello and welcome</h1>
+<footer>
+This project was coded by Natali and is 
+    <a href="https://github.com/Nwakomi/official-weather-app" target="_blank"> open-sourced in Github</a>
+    </footer>
     </div>
   );
 }
